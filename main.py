c715f6875cc3d6f01a473c48e3921c939411ac64
programmingvenkat <venkatiaf04@gmail.com>
2017-09-02 13:27:37 +0530

    better error commands and bugfixes

--- main.py
+++ main.py
@@ -25,43 +25,47 @@ help_handler = CommandHandler('help', help)
 dispatcher.add_handler(help_handler)
 #######################################
 def getWikiSummary(q):
-        query = q.replace('/summary', '')
-        qeury = q.replace('@i_swear_i_dont_spam_bot', '')
+        query = q.replace('/summary', '').replace('@i_swear_i_dont_spam_bot', '')
+        
         if len(query) < 2:
             return 'What would you like to get a summary about? For example /summary magnetism'
         else:
-            return wikipedia.summary(query)
-   
+            return wikipedia.summary(query) 
+            
+  
 def wikiLink(bot, update):
     try:
+        
         summaryFinal = getWikiSummary(update.message.text)
+        
         bot.send_message(chat_id=update.message.chat_id, text=summaryFinal)
     except:
+
         bot.send_message(chat_id=update.message.chat_id, text='Sorry! Too many or no articles regarding that :\'( . Use /search to see if the article exists.')
 
 wikiHandler = CommandHandler('summary', wikiLink)
 dispatcher.add_handler(wikiHandler)
 
 def getWikiSearch(q):
-    query = q.replace('/search', '') #remove command and username mention parts
-    qeury = q.replace('@i_swear_i_dont_spam_bot', '')
+    query = q.replace('/search', '').replace('@i_swear_i_dont_spam_bot', '') #remove command and username mention parts
+    
     if len(query) < 2:
         return ['What would you like to search about? For example /search magnetism']
     else:
-        return '*\n'+wikipedia.search(query)+'*\n'
+        return  wikipedia.search(query)  
 def wikiSearch(bot, update):
     try:
         finalResults = ''
         searchResults = getWikiSearch(update.message.text)
         
         finalResults = ', \n'.join(searchResults)
-        bot.send_message(chat_id=update.message.chat_id, text='_RESULTS:_'+finalResults+'Use /summary for example to get a summary', parse_mode=telegram.ParseMode.MARKDOWN)
+        bot.send_message(chat_id=update.message.chat_id, text='\n*'+finalResults+'\n*'+'Use /summary for example to get a summary', parse_mode=telegram.ParseMode.MARKDOWN)
     except:  
         bot.send_message(chat_id=update.message.chat_id, text='Sorry! Couldn\'t find anything matching that :\'(. Try a more general search?')
 search_handler = CommandHandler('search', wikiSearch)
 dispatcher.add_handler(search_handler)
 
-
+#updater.start_polling()
 PORT = int(os.environ.get('PORT', '5000'))
 updater.start_webhook(listen='0.0.0.0', port=PORT, url_path='443409571:AAEUtkX0T74kJFotCl57ORJRJBl-fBeZQ-Q')
 updater.bot.setWebhook("https://the-anti-spammer.herokuapp.com/" + '443409571:AAEUtkX0T74kJFotCl57ORJRJBl-fBeZQ-Q')
